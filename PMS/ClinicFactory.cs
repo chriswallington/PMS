d304d2a6bd6bdf779a54b8de9d4b2c887951a233
Chris Wallington <chris@carpedatum.co.uk>
2019-04-12 15:38:35 +0100

    Add project files.

---
+++ PMS/ClinicFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS
+{
+    public class ClinicFactory : TransactionFactory
+    {
+        public override ITransaction Create(int transactionId) => new ClinicTransaction();
+    }
+}
---
+++ PMS/VisitTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS
+{
+    public class VisitTransaction : ITransaction
+    {
+        public void CreateTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
